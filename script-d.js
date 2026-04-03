dbf9e687cc44dee61c9aee5917d9027c523909ee
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2026-04-03 18:11:17 +0700

    fixed links in script d

--- script-d.js
+++ script-d.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const links = document.getElementById("links");
   links.innerHTML = `
     <ul>
-      <li><a href="/js-crawlability-experiment/page-a.html">JS-added link to Page A</a></li>
-      <li><a href="/js-crawlability-experiment/page-b.html">JS-added link to Page B</a></li>
-      <li><a href="/js-crawlability-experiment/page-c.html">JS-added link to Page C</a></li>
+      <li><a href="/page-a.html">JS-added link to Page A</a></li>
+      <li><a href="/page-b.html">JS-added link to Page B</a></li>
+      <li><a href="/page-c.html">JS-added link to Page C</a></li>
     </ul>
     <p><strong>Unique test sentence:</strong> These internal links were inserted with JavaScript only.</p>
   `;
